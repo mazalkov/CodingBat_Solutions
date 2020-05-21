edaaa0620c32adbfd9b1345ca85a6aac38a931c0
Andrew <32670729+anzdrew@users.noreply.github.com>
2020-05-22 00:52:24 +0100

    Create sum13.py

---
+++ List-2/sum13.py
@@ -0,0 +1,16 @@
+# Return the sum of the numbers in the array, returning 0 for an empty array. 
+# Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+
+def sum13(nums):
+  
+  i = 0
+  sum = 0
+  
+  while i < len(nums):
+    if nums[i] == 13:
+      i += 1
+    else:
+      sum += nums[i]
+    i += 1
+    
+  return sum
